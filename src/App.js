bc2e20ff384f02043268a8f700694b67d698931a
João Henrique Barbosa <joaohenriquebarbosa21@gmail.com>
2022-04-19 17:02:58 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onAdd = () => {
+    setTodos([...todos,
+    {
+      title: todoTitle,
+      checked: false
+    }
+    ]);
+    setTodoTitle("");
+  }
+
+  const onCheckboxChange = (checked, index) => {
+    setTodos(todos.map((todo, _index) => index === _index ? ({ ...todo, checked }) : todo));
+  }
+
+  const onSend = () => {
+    console.log(todos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Todo App Emily</h1>
       </header>
+      <div class="todo-body">
+        <input
+          type="text"
+          placeholder="Título da tarefa"
+          value={todoTitle}
+          onChange={(ev) => setTodoTitle(ev.target.value)}
+          onKeyDown={(ev) => ev.key === "Enter" && onAdd()}
+        />
+        <button className="add-button" onClick={onAdd}>+</button>
+        <ul>
+          {
+            todos.map((todo, index) => {
+              return (
+                <li style={todo.checked ? {textDecoration: "line-through"} : {}}>
+                  <input
+                    type="checkbox"
+                    checked={todo.checked}
+                    onChange={(ev) => onCheckboxChange(ev.target.checked, index)}
+                  />
+                  {todo.title}
+                </li>
+              )
+            })
+          }
+        </ul>
+        <footer>
+          <button onClick={onSend}>Enviar</button>
+        </footer>
+      </div>
     </div>
   );
 }
