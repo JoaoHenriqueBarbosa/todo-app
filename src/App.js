aa21c9a54a5115aa0f3437b3bc2dd1c56ceddf36
JoaoHenriqueBarbosa <joaohenriquebarbosa21@gmail.com>
2022-05-31 17:07:13 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
-  const [todoTitle, setTodoTitle] = useState("");
+  const [todoTitle, setTodoTitle] = useState('');
   const [todos, setTodos] = useState([]);
 
   const onAdd = () => {
-    setTodos([...todos,
-    {
-      title: todoTitle,
-      checked: false
-    }
+    setTodos([
+      ...todos,
+      {
+        title: todoTitle,
+        checked: false,
+      },
     ]);
-    setTodoTitle("");
-  }
+    setTodoTitle('');
+  };
 
   const onCheckboxChange = (checked, index) => {
-    setTodos(todos.map((todo, _index) => index === _index ? ({ ...todo, checked }) : todo));
-  }
+    setTodos(
+      todos.map((todo, _index) =>
+        index === _index ? { ...todo, checked } : todo
+      )
+    );
+  };
 
   const onSend = () => {
-    console.log(todos)
-  }
+    console.log(todos);
+  };
 
   return (
     <div className="app">
@@ -35,24 +39,26 @@ function App() {
           placeholder="Título da tarefa"
           value={todoTitle}
           onChange={(ev) => setTodoTitle(ev.target.value)}
-          onKeyDown={(ev) => ev.key === "Enter" && onAdd()}
+          onKeyDown={(ev) => ev.key === 'Enter' && onAdd()}
         />
-        <button className="add-button" onClick={onAdd}>+</button>
+        <button className="add-button" onClick={onAdd}>
+          +
+        </button>
         <ul>
-          {
-            todos.map((todo, index) => {
-              return (
-                <li style={todo.checked ? {textDecoration: "line-through"} : {}}>
-                  <input
-                    type="checkbox"
-                    checked={todo.checked}
-                    onChange={(ev) => onCheckboxChange(ev.target.checked, index)}
-                  />
-                  {todo.title}
-                </li>
-              )
-            })
-          }
+          {todos.map((todo, index) => {
+            return (
+              <li
+                style={todo.checked ? { textDecoration: 'line-through' } : {}}
+              >
+                <input
+                  type="checkbox"
+                  checked={todo.checked}
+                  onChange={(ev) => onCheckboxChange(ev.target.checked, index)}
+                />
+                {todo.title}
+              </li>
+            );
+          })}
         </ul>
         <footer>
           <button onClick={onSend}>Enviar</button>
